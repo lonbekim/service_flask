34a0f79a0c664595836bfc4eb5efd81e561cffaf
lonbekim <lonbekim@gmail.com>
2018-01-27 02:47:33 -0500

    add template file and directory

--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
-import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!!!'
+def main():
+	return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
 
 if __name__ == '__main__':
     app.run(debug=True)
